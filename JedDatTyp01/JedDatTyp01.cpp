cebcd6b37b85542e39bd2f21eadc17b9121380b6
dostalm.04 <dostalm.04@spst.eu>
2022-10-20 13:26:04 +0200

    Přidejte soubory projektu.

---
+++ JedDatTyp01/JedDatTyp01.cpp
@@ -0,0 +1,56 @@
+﻿// JedDatTyp01.cpp : Tento soubor obsahuje funkci main. Provádění programu se tam zahajuje a ukončuje.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	char x;
+	int soucet = 0;
+
+
+
+
+
+	cout << "Zadej pocet prvku posloupnosti: ";
+
+	cin >> n;
+
+	cout << "\nZadavej znaky posloupnosti: " << endl;
+	
+	bool jeVetsi = false;
+	int i = 0;
+	while (i < n && !jeVetsi)
+	{
+		cin >> x;
+		cout << "Zadany znak je: " << x << endl; 
+		soucet += x;
+
+		if (soucet > 1000)
+		{
+			jeVetsi = true;
+		}
+
+
+
+
+		i++;
+	}
+
+	if (jeVetsi)
+	{
+		cout << "\nAno soucet znaku je vetsi nez 1000 a je: " << soucet <<endl <<endl;
+	}
+	else
+	{
+		cout << "\nNe soucet neni vetsi nez 1000" << endl << endl;
+	}
+		
+
+
+
+}
+
+
